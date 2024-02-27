310baa2f8a56be397216dd99b4e5ad7bc021bf00
Fazlı Altun <altunfazl@gmail.com>
2024-02-27 14:28:14 +0300

    created a design for extension

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import "./App.css";
-
+import { useState } from "react";
+import logo from "./moralisLogo.svg"
+import { Select } from "antd";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-  
+  const [selectedChain, setSelectedChain] = useState("0x1");
 
   return (
     <div className="App">
+      <header>
+        <img src={logo} className="headerLogo" alt="logo" />
+        <Select
+          onChange={(val) => setSelectedChain(val)}
+          value={selectedChain}
+          options={[
+            {
+              label: "Ethereum",
+              value: "0x1"
+            },
+            {
+              label: "Mumbai Testnet",
+              value: "0x13881"
+            },
+            {
+              label: "Polygon",
+              value: "0x89"
+            },
+            {
+              label: "Avalance",
+              value: "0xa86a"
+            }
+          ]}
+          className="dropdown"
+        ></Select>
+      </header>
+      <Routes>
+        <Route path = "/" element={ <Home/>}/>
+      </Routes>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,31 @@
 import React from "react";
+import mwallet from "../mwallet.png";
+import { Button } from "antd";
 
 function Home() {
 
   return (
     <>
       <div className="content">
+        <img src={mwallet} alt="logo" className="frontPageLogo" />
+        <h2>Hey There 🦅</h2>
+        <h4 className="h4">Welcome to our Web3 Wallet 🦅</h4>
+        <Button
+          className="frontPageButton"
+          type="primary">
+          Create a Wallet
+        </Button>
+        <Button
+          className="frontPageButton"
+          type="default">
+          Sign In with Seed Phrase
+        </Button>
+        <p className="frontPageBottom">
+          Buraya kendi linkimizi koyabiliriz: {" "}
+          <a href="https://moralismoney.com/" target="_blank" rel="noreferrer">
+            money.moralis.io
+          </a>
+        </p>
       </div>
     </>
   );
